b9b4a4b130befa7fdca24515c32e522ccab3ca91
Vitaly _Vi Shukela <vi0oss@gmail.com>
2021-01-17 01:18:02 +0300

    Remove excessive duplicate documentation

--- gaborator-sys/README.md
+++ gaborator-sys/README.md
@@ -1,32 +1,9 @@
 # gaborator-sys
 
-Gaborator is a C++ library for converting audio samples to a special spectral representation
-that uses different FTT sizes based on whether it is bass or treble (oversimplifying here).
-The transformation is reversible.
-See [the website](https://www.gaborator.com/) for more info.
+Low-level part of `gaborator` crate.
 
-This crate is a [cxx](https://cxx.rs/)-based wrapper of this library, allowing Rust code to use Gaborator (although with reduced efficiency).
+Documentation was removed from it, as it mostly dulicate the `gaborator`'s one.
 
-Limitations:
-
-* `f32` only
-* Not performance-minded
-* Some overridable or low-level details not exposed
-* No visualisation
-* Crate soundness may be iffy - I was just followed the path of least resistance.
-* Arithmentic overflows in buffer length calculations are not checked.
-* Not really tested, apart from included examples. For example, streaming should be supported, but I haven't tried it myself.
-
-Currently based on Gaborator version 1.6. Source code of the Gaborator is included into the crate.
-
-Availble examples:
-
-* Phase information randomizer, creating sort-of-reverberation audio effect.
-* Converts the analyzed sound to (sample,band,magnitude,phase) CSV file and back.
-
-License of Gaborator is Affero GPL 3.0.
-
-Glue code (sans doccomments copied from Gaborator) in this crate may be considered
-to be licensed as either MIT or AGPL-3.0, at your option.
+There are two examples, but they do the same thing as the ones included in `gaborator` crate.
 
 License: AGPL-3.0
--- gaborator-sys/src/lib.rs
+++ gaborator-sys/src/lib.rs
@@ -1,38 +1,16 @@
-//! Gaborator is a C++ library for converting audio samples to a special spectral representation
-//! that uses different FTT sizes based on whether it is bass or treble (oversimplifying here).
-//! The transformation is reversible.
-//! See [the website](https://www.gaborator.com/) for more info.
+//! Low-level part of `gaborator` crate.
 //!
-//! This crate is a [cxx](https://cxx.rs/)-based wrapper of this library, allowing Rust code to use Gaborator (although with reduced efficiency).
+//! Documentation was removed from it, as it mostly dulicate the `gaborator`'s one.
 //!
-//! Limitations:
-//!
-//! * `f32` only
-//! * Not performance-minded
-//! * Some overridable or low-level details not exposed
-//! * No visualisation
-//! * Crate soundness may be iffy - I was just followed the path of least resistance.
-//! * Arithmentic overflows in buffer length calculations are not checked.
-//! * Not really tested, apart from included examples. For example, streaming should be supported, but I haven't tried it myself.
-//!
-//! Currently based on Gaborator version 1.6. Source code of the Gaborator is included into the crate.
-//! 
-//! Availble examples:
-//! 
-//! * Phase information randomizer, creating sort-of-reverberation audio effect.
-//! * Converts the analyzed sound to (sample,band,magnitude,phase) CSV file and back.
-//!
-//! License of Gaborator is Affero GPL 3.0.
-//!
-//! Glue code (sans doccomments copied from Gaborator) in this crate may be considered
-//! to be licensed as either MIT or AGPL-3.0, at your option.
+//! There are two examples, but they do the same thing as the ones included in `gaborator` crate.
 
-#![deny(missing_docs)]
+#[deny(missing_docs)]
 
 pub extern crate cxx;
 
 #[cxx::bridge(namespace = "gabbridge")]
 mod ffi {
+    #[deny(missing_docs)] // pub-reexported by the high-level crate 
     /// Corresponds to `gaborator::parameters`.
     pub struct Params {
         /// The number of frequency bands per octave.
@@ -60,6 +38,7 @@ mod ffi {
         pub overlap: f64,
     }
 
+    #[deny(missing_docs)]
     /// Complex point, representing one coefficient.
     /// Magnitude is loudness at this point, argument is phase.
     #[derive(Copy,Clone,Debug,PartialEq,PartialOrd,Default)]
@@ -70,6 +49,7 @@ mod ffi {
         pub im: f32,
     }
 
+    #[deny(missing_docs)]
     /// Additional data for `read_coefficients_with_meta` or `write_coefficients_with_meta`
     #[derive(Copy,Clone,Debug,PartialEq,PartialOrd,Default,Eq,Ord,Hash)]
     pub struct CoefMeta {
@@ -90,49 +70,19 @@ mod ffi {
     unsafe extern "C++" {
         include!("gaborator-sys/src/gabbridge.h");
 
-        /// Represents C++'s `gaborator::analyzer<float>`. Use `new_analyzer` function to create it.
         pub type Analyzer;
-
-        /// Reprepresents C++'s `gaborator::coefs<float>`. Create it using `create_coefs`.
-        /// Can be memory-hungry.
-        /// 
-        /// (I'm not sure whether this can be dropped after `Analyzer`.
-        /// I see some mention of reference counting withing Gaborator library,
-        /// but have not checked in detail.)
         pub type Coefs;
 
-        /// Create new instance of Gaborator analyzer/synthesizer based on supplied parameters
         pub fn new_analyzer(params: &Params) -> UniquePtr<Analyzer>;
 
-        /// Returns the one-sided worst-case time domain support of any of the analysis filters.
-        /// When calling `analyze()` with a sample at time t, only spectrogram coefficients within
-        /// the time range t ± support will be significantly changed. Coefficients outside the range
-        /// may change, but the changes will sufficiently small that they may be ignored without significantly reducing accuracy.
         pub fn get_analysis_support_len(b: &Analyzer) -> usize;
-
-        /// Returns the one-sided worst-case time domain support of any of the reconstruction filters.
-        /// When calling synthesize() to synthesize a sample at time t, the sample will only be significantly
-        /// affected by spectrogram coefficients in the time range t ± support. Coefficients outside the range
-        /// may be used in the synthesis, but substituting zeroes for the actual coefficient values will not significantly reduce accuracy.
         pub fn get_synthesis_support_len(b: &Analyzer) -> usize;
 
-        /// Create `Coefs` - the holder of analyzed data. 
         pub fn create_coefs(b: &Analyzer) -> UniquePtr<Coefs>;
 
-        /// Allow the coefficients for points in time before limit 
-        /// (a time in units of samples) to be forgotten.
-        /// Streaming applications can use this to free memory used by coefficients
-        /// that are no longer needed. Coefficients that have been forgotten will read as zero.
-        /// This does not guarantee that all coefficients before limit are forgotten, only that
-        /// ones for limit or later are not, and that the amount of memory consumed by
-        /// any remaining coefficients before limit is bounded.
         pub fn forget_before(b: &Analyzer, c: Pin<&mut Coefs>, limit: i64, clean_cut: bool);
 
 
-        /// Read or write values within `Coefs`, skipping over non-existent entries.
-        /// Corresponds to `process` function of Gaborator.
-        /// `from_band` and `to_band` may be given INT_MIN / INT_MAX values, that would mean all bands.
-        /// `from_sample_time` and `to_sample_time` can also be given INT64_MIN / INT64_MAX value to mean all available data.
         pub fn process(
             coefs: Pin<&mut Coefs>,
             from_band: i32,
@@ -142,10 +92,6 @@ mod ffi {
             callback: &mut ProcessOrFillCallback,
         );
 
-        /// Write values to `Coefs`, creating non-existent entries as needed.
-        /// Corresponds to `fill` function of Gaborator.
-        /// `from_band` and `to_band` may be given INT_MIN / INT_MAX values, that would mean all bands.
-        /// `from_sample_time` / `to_sample_time` should not be set to overtly large range, lest memory will be exhausted.
         pub fn fill(
             coefs: Pin<&mut Coefs>,
             from_band: i32,
@@ -155,10 +101,6 @@ mod ffi {
             callback: &mut ProcessOrFillCallback,
         );
 
-        /// Spectrum analyze the samples at `signal` and add the resulting coefficients to `coefs`.
-        /// `t1` parameter from Gaborator's `analyze` method is caluclated based on supplied slice size.
-        ///
-        /// If the `coefs` object already contains some coefficients, the new coefficients are summed to those already present.
         pub fn analyze(
             b : &Analyzer,
             signal: &[f32],
@@ -166,11 +108,6 @@ mod ffi {
             coefs: Pin<&mut Coefs>,
         );
             
-        /// Synthesize signal samples from the coefficients `coef` and store them at `signal`. 
-        /// `t1` parameter from Gaborator's `synthesize` method is caluclated based on supplied slice size.
-        /// 
-        /// The time range may extend outside the range analyzed using analyze(), in which case
-        /// the signal is assumed to be zero in the un-analyzed range.
         pub fn synthesize(
             b : &Analyzer,
             coefs: &Coefs,
@@ -178,26 +115,14 @@ mod ffi {
             signal: &mut [f32],
         );
 
-        /// Return the smallest valid bandpass band number, corresponding to the highest-frequency bandpass filter.
-        /// 
-        /// The frequency bands of the analysis filter bank are numbered by nonnegative integers that
-        /// increase towards lower (sic) frequencies. There is a number of bandpass bands corresponding
-        /// to the logarithmically spaced bandpass analysis filters, from near 0.5 (half the sample rate)
-        /// to near fmin, and a single lowpass band containing the residual signal from frequencies below fmin.
         pub fn  bandpass_bands_begin(b : &Analyzer) -> i32;
 
-        /// Return the bandpass band number one past the highest valid bandpass band number,
-        /// corresponding to one past the lowest-frequency bandpass filter. 
         pub fn  bandpass_bands_end(b : &Analyzer) -> i32;
 
-        /// Return the band number of the lowpass band. 
         pub fn  band_lowpass(b : &Analyzer)  -> i32;
 
-        /// Return the band number corresponding to the reference frequency `ff_ref`.
-        /// If `ff_ref` falls within the frequency range of the bandpass filter bank, this will be a valid bandpass band number, otherwise it will not. 
         pub fn  band_ref(b : &Analyzer) -> i32;
 
-        /// Return the center frequency of band number `band`, in units of the sampling frequency. 
         pub fn  band_ff(b : &Analyzer, band: i32) -> f64;
     }
 }
